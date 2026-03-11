e05ee3288201d84e2b2542e7c8d06b06f8e364ff
SOHAILASIF1 <alyana998877@gmail.com>
2026-03-11 12:49:57 +0500

    shop discount

--- LECTURE 2 Conditionals/Index.js
+++ LECTURE 2 Conditionals/Index.js
@@ -1,11 +1,35 @@
-// Valid Voter
-let age = 18
-let havaCNIC=true
-if (age>=18 && havaCNIC) {
-    console.log("Allow For Voting");
+// // Valid Voter
+// let age = 18
+// let havaCNIC=true
+// if (age>=18 && havaCNIC) {
+//     console.log("Allow For Voting");
     
     
-}else {
-    console.log("Not Allow for Voting");
+// }else {
+//     console.log("Not Allow for Voting");
     
+// }
+
+// shop Discount 
+let shoping=10000
+if (shoping>0&&shoping<=5000) {
+    let Discount=0
+    let res=(Discount/100)*shoping
+   let final =shoping-res
+   console.log(final);
+   
+    
+    
+}else if (shoping>5000 && shoping<=7000) {
+       let Discount=5
+    let res=(Discount/100)*shoping
+   let final =shoping-res
+   console.log(final);
+    
+} else if(shoping>7000&&shoping<=10000){
+       let Discount=20
+    let res=(Discount/100)*shoping
+   let final =shoping-res
+   console.log(final);
+
 }
