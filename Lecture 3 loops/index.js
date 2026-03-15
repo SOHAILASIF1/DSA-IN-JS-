b796bffb553fc27456161bab5d039147accc9a64
SOHAILASIF1 <alyana998877@gmail.com>
2026-03-15 14:03:42 +0500

    reverse a number

--- Lecture 3 loops/index.js
+++ Lecture 3 loops/index.js
@@ -34,41 +34,85 @@
 // console.log(factorial);
 
 
-// factor of a numbers
-let pr = prompt("Enter A Number")
-// console.log(pr);
-if (pr === null) {
-    console.log("Canselled");
+// // factor of a numbers
+// let pr = prompt("Enter A Number")
+// // console.log(pr);
+// if (pr === null) {
+//     console.log("Canselled");
 
 
+// }
+
+// let num = Number(pr)
+
+
+// if (isNaN(num)) {
+//     console.log("Invalid Choice PLease Select a number");
+
+
+// } else {
+//     if (num > 0) {
+//         let factor = []
+//         for (let index = 1; index <= num; index++) {
+//             if (num % index === 0) {
+//                 factor.push(index)
+
+//             }
+
+
+//         }
+//         console.log(factor);
+
+//     } else {
+//         console.log("Should be +ve number and greater then zero");
+
+//     }
+
+// }
+
+// //prime number
+
+// let num=7
+// let prime=true
+// for (let index = 2; index < Math.floor(num/2); index++) {
+//     if (num%index===0) {
+//         prime=false
+//         break;
+        
+//     }
+    
+// }
+// console.log(prime);
+
+// // sum of digit
+// let n=23465
+//  let sum=0
+//  while(n>0){
+//     let rem=n%10
+//     sum+=rem
+//     n=Math.floor(n/10)
+
+//  }
+//  console.log(sum);
+
+//reverse a number
+let n=23456
+let rev=0   //the equation of reverse is rev=rev*10+rem 
+while(n>0){
+    let rem=n%10
+    rev=rev*10+rem
+    n=Math.floor(n/10)
 }
-
-let num = Number(pr)
-
-
-if (isNaN(num)) {
-    console.log("Invalid Choice PLease Select a number");
+console.log(rev);
 
 
-} else {
-    if (num > 0) {
-        let factor = []
-        for (let index = 1; index <= num; index++) {
-            if (num % index === 0) {
-                factor.push(index)
-
-            }
+ 
 
 
-        }
-        console.log(factor);
 
-    } else {
-        console.log("Should be +ve number and greater then zero");
 
-    }
 
-}
+
 
 
 
