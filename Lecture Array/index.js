f3b20c3d7828013004a20aeeaaac432728eb9495
SOHAILASIF1 <alyana998877@gmail.com>
2026-03-23 09:07:33 +0500

    sdhgj

--- Lecture Array/index.js
+++ Lecture Array/index.js
@@ -65,7 +65,15 @@
 
 
 
-
-
+// reverse a array
+let arr=[1,2,3,4,5,6,7,8,9,10]
+let revArr=[]
+for (let index = arr.length -1 ; index >= 0; index--) {
+    console.log(index);
+    
+    revArr.push(arr[index])
+    
+}
+console.log(revArr);
 
 
