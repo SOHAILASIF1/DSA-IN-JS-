946d3c21e44649e9477e33be4c5cb71ff1d3b241
SOHAILASIF1 <alyana998877@gmail.com>
2026-03-23 08:53:34 +0500

    jhdsk

---
+++ Lecture 4 Array/index.js
@@ -0,0 +1,71 @@
+// let array=[10,20,40,50,60]
+//  let sum=0
+//  for (let index = 0; index < array.length; index++) {
+//      sum += array[index];
+    
+//  }
+// console.log(sum);
+ //max element
+
+//  let array=[10,3780,80,90]
+//  let max=array[0]
+//  for (let index = 0; index < array.length; index++) {
+//     if (array[index]>max) {
+//          max = array[index];
+//     }
+    
+//  }
+//  console.log(max);
+
+// //min elem
+// let array=[43,98,76,67]
+// let min=array[0]
+// for (let index = 0; index < array.length; index++) {
+//     if (array[index]<min) {
+
+//       min=array[index]
+//     }
+    
+// }
+//  console.log(min);
+
+// //second max 
+// let array=[1,2,3,4,5,29,8,17,8,91]
+// let max;
+// let secMax;
+// if (array[0]>array[1]) {
+//     max=array[0]
+//     secMax=array[1]
+    
+// }else{
+//     secMax=array[0]
+//     max=array[1]
+// }
+ 
+// for (let i = 2; i < array.length; i++) {
+//    if (array[i]>max ) {
+//     secMax=max
+//      max=array[i]
+//    }else if(array[i]>secMax &&max !==array[i]){
+//     secMax=array[i]
+//    }
+  
+  
+    
+// }
+// console.log(secMax);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
