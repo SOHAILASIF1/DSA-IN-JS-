299cfa124114ab52e65c718cc053ea6068d1bfcc
SOHAILASIF1 <alyana998877@gmail.com>
2026-03-25 23:31:26 +0500

    fhdjk

--- Lecture Advance Array/index.js
+++ Lecture Advance Array/index.js
@@ -71,3 +71,16 @@
 
 
 //duplicate in sorted array
+
+let arr=[0,0,1,1,1,2,3,3,4,5,6]
+let j=1
+
+for (let i = 0; i < arr.length-1; i++) {
+   
+    if (arr[i]!=arr[i+1]) {
+        arr[j]=arr[i+1]
+        j++
+    }
+   
+}
+console.log(arr);
