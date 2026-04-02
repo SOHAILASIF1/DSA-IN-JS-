be9329134f840b68ac75a5bef3ec83f55b90c9aa
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-02 23:05:15 +0500

    selection sort

--- Lecture Sorting Alogorithm/index.js
+++ Lecture Sorting Alogorithm/index.js
@@ -1,17 +1,43 @@
 //Bubble Sort
 
-let arr=[1,4,2,3,5,9,6]
+// let arr=[1,4,2,3,5,9,6]
+// let n=arr.length
+// for (let i = 0; i < n-1; i++) {
+//     for (let j = 0; j < n-1-i; j++) {
+//        if (arr[j]>arr[j+1]) {
+//         let temp=arr[j]
+//         arr[j]=arr[j+1]
+//         arr[j+1]=temp
+        
+//        }
+        
+//     }
+    
+// }
+// console.log(arr);
+
+//selection sort
+let arr=[2,3,14,5,6]
 let n=arr.length
 for (let i = 0; i < n-1; i++) {
-    for (let j = 0; j < n-1-i; j++) {
-       if (arr[j]>arr[j+1]) {
-        let temp=arr[j]
-        arr[j]=arr[j+1]
-        arr[j+1]=temp
+    let minIndx=i    //0
+    for (let j = i+1; j < n; j++) {
+        if (arr[minIndx]>arr[j]) {
+            minIndx=j
+            
+        }
+        console.log(minIndx);
         
-       }
+       
         
     }
+     if (minIndx!=i) {
+            let temp=arr[minIndx]
+             arr[minIndx]=arr[i]
+             arr[i]=temp
+            
+        }
     
 }
+
 console.log(arr);
