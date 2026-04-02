612a8c9f97625567340fd6a9bd75ac6c9fd65cef
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-02 22:31:26 +0500

    bubble sort

---
+++ Lecture Sorting Alogorithm/index.js
@@ -0,0 +1,17 @@
+//Bubble Sort
+
+let arr=[1,4,2,3,5,9,6]
+let n=arr.length
+for (let i = 0; i < n-1; i++) {
+    for (let j = 0; j < n-1-i; j++) {
+       if (arr[j]>arr[j+1]) {
+        let temp=arr[j]
+        arr[j]=arr[j+1]
+        arr[j+1]=temp
+        
+       }
+        
+    }
+    
+}
+console.log(arr);
