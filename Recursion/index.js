898f6f4e80eaa4c8893ec6ae368f4b4ceda2601a
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-04 07:55:40 +0500

    guyihy

--- Recursion/index.js
+++ Recursion/index.js
@@ -8,11 +8,22 @@
 
 
 //print number n to 1
-function printNum(n){
-    if (n===0) return
-    console.log(n);
-    printNum(--n)    
+// function printNum(n){
+//     if (n===0) return
+//     console.log(n);
+//     printNum(--n)    
 
+
+// }
+// printNum(10000)
+
+//print natural number 1 to n
+
+function printNum(n){
+    if(n==0)return
+    printNum(n-1)
+    console.log(n);
+    
 
 }
-printNum(10000)
+printNum(1000)
