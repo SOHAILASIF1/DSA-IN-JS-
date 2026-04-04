4948621e113f67bebc1ebebb4fe84c5afa2959fd
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-04 07:44:30 +0500

    fyg

--- Recursion/index.js
+++ Recursion/index.js
@@ -1,7 +1,18 @@
-function print(n){
-    if (n===0) return
-    console.log("Hi",n);
-    print(n-1)
+// function print(n){
+//     if (n===0) return
+//     console.log("Hi",n);
+//     print(n-1)
     
+// }
+// print(7)
+
+
+//print number n to 1
+function printNum(n){
+    if (n===0) return
+    console.log(n);
+    printNum(--n)    
+
+
 }
-print(7)
+printNum(10000)
