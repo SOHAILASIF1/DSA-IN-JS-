40f5b4332fd3b495677e496d97dd384ad27eb585
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-15 07:10:42 +0500

    missing number

--- cyclic sort/index.js
+++ cyclic sort/index.js
@@ -16,18 +16,38 @@
 // console.log(arr);
 
 //0 to n-1 [0,n-1]
-let arr=[2,0,5,8,11,12,9,10,4,3,7,1,6]
-let i=0
+// let arr=[2,0,5,8,11,12,9,10,4,3,7,1,6]
+// let i=0
+// while (i<arr.length) {
+//     let corInd=arr[i]
+//     if (arr[i]!=arr[corInd]) {
+//         let temp=arr[i]
+//         arr[i]=arr[corInd]
+//         arr[corInd]=temp
+        
+//     }
+//     else i++
+    
+// }
+// console.log(arr);
+
+//missing number find
+let arr=[2,3,0]
+let i =0
 while (i<arr.length) {
-    let corInd=arr[i]
-    if (arr[i]!=arr[corInd]) {
+    let corIdx=arr[i]
+    if (arr[i]<arr.length && arr[i]!=arr[corIdx]) {
         let temp=arr[i]
-        arr[i]=arr[corInd]
-        arr[corInd]=temp
+        arr[i]=arr[corIdx]
+        arr[corIdx]=temp
         
     }
-    else i++
+    for (let j = 0; j < arr.length; j++) {
+       if (j!=arr[j]) return j
+        
+    }
+    return arr.length
     
 }
-console.log(arr);
+console.log(j);
 
