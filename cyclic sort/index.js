6d3d057630b2951b1884acc3422eef53459b931b
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-15 23:01:52 +0500

    missing array number and return

--- cyclic sort/index.js
+++ cyclic sort/index.js
@@ -31,23 +31,51 @@
 // }
 // console.log(arr);
 
-//missing number find
-let arr=[2,3,0]
-let i =0
+// let arr = [2, 3, 0];
+// let i = 0;
+
+// while (i < arr.length) {
+
+//   let corIdx = arr[i];
+//   if (arr[i] < arr.length && arr[i] !== arr[corIdx]) {
+//     [arr[i], arr[corIdx]] = [arr[corIdx], arr[i]];
+//   } else {
+//     i++;
+//   }
+// }  // ← sort complete before checking
+
+
+// for (let j = 0; j < arr.length; j++) {
+
+//   if (j !== arr[j]) return j;
+// }
+
+// return arr.length;
+
+
+//find the array of missing element
+let arr=[4,3,2,7,8,2,3,1]
+/*
+[1,2,2,3,3,4,7,8]
+*/
+let i=0
+let temparr=[]
 while (i<arr.length) {
-    let corIdx=arr[i]
-    if (arr[i]<arr.length && arr[i]!=arr[corIdx]) {
-        let temp=arr[i]
-        arr[i]=arr[corIdx]
-        arr[corIdx]=temp
-        
-    }
-    for (let j = 0; j < arr.length; j++) {
-       if (j!=arr[j]) return j
-        
-    }
-    return arr.length
-    
+  let curIndex=arr[i]-1
+  if (arr[i]<=arr.length&&arr[i]!=arr[curIndex]) {
+    let temp=arr[i]
+    arr[i]=arr[curIndex]
+    arr[curIndex]=temp
+  }else i++
+  
+  
 }
-console.log(j);
+for (let j = 0; j < arr.length; j++) {
+   if ((j+1)!=arr[j]) {
+    temparr.push(j+1)
+   }
+    
+  }
+console.log(temparr);
+
 
