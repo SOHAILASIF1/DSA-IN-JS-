8130e08e2eaef13a0d9d4b4d032b48269c946389
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-15 23:36:19 +0500

    hdusij

--- search insert position/index.js
+++ search insert position/index.js
@@ -1,12 +1,12 @@
-function binarySerach(arr,first,last,target) {
+function binarySearch(arr,first,last,target) {
     while (first<=last) {
         let mid=Math.floor((first+last)/2)
         if (arr[mid]==target) return mid
-        if (arr[mid]<target) first=mid+1
+        else if (arr[mid]<target) first=mid+1
         else last=mid-1
     }
     return first
     
 }
 let arr=[1,3,5,6]
-console.log(binarySerach(arr,0,arr.length,4));
+console.log(binarySearch(arr,0,arr.length-1,4));
