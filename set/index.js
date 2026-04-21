32d6ed6664bd21a04d3b7fb322063cb34dbe219a
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-21 09:19:32 +0500

    ghdfjk

--- set/index.js
+++ set/index.js
@@ -31,13 +31,51 @@
 // }
 
 // frequncy found of element
-let arr=[12,3,2,1,2,3,2,1,2,4,5,6,7,6,5]
+// let arr=[12,3,2,1,2,3,2,1,2,4,5,6,7,6,5]
+// let map=new Map()
+// for (let i = 0; i < arr.length; i++) {
+//     if (map.has(arr[i])) {
+//        map.set(arr[i],map.get(arr[i])+1)
+//     }else map.set(arr[i],1)
+    
+// }
+// console.log(map);
+
+//sort thr people
+// let height=[123,167,189]
+// let people=["sohail","ali","rohail"]
+
+// let map=new Map()
+// for (let i = 0; i < height.length; i++) {
+//   map.set(height[i],people[i])
+    
+// }
+// height.sort((a,b)=>b-a)
+// for (let i = 0; i < people.length; i++) {
+//     people[i]=map.get(height[i])
+    
+// }
+
+
+// console.log(map);
+
+
+//two sum
+let arr=[11,4,8,19]
+let target=12
 let map=new Map()
 for (let i = 0; i < arr.length; i++) {
-    if (map.has(arr[i])) {
-       map.set(arr[i],map.get(arr[i])+1)
-    }else map.set(arr[i],1)
+    if (map.has(target-arr[i])) {
+         console.log([i,map.get(target-arr[i])])
+         
+       
+    }else map.set(arr[i],i)
     
 }
-console.log(map);
+
+
+
+
+
+
 
