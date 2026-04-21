19464b8cc77e32991d4faac66ecccd7d4669b072
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-21 06:52:51 +0500

    sdfjk

--- set/index.js
+++ set/index.js
@@ -1,11 +1,31 @@
 //print the unique vale
-let arr=[12,34,12,56,43,56,43]
-let set=new Set()
-for (let i = 0; i < arr.length; i++) {
-   if (set.has(arr[i])) {
-    set.delete(arr[i])
+// let arr=[12,34,12,56,43,56,43]
+// let set=new Set()
+// for (let i = 0; i < arr.length; i++) {
+//    if (set.has(arr[i])) {
+//     set.delete(arr[i])
     
-   } else set.add(arr[i])
+//    } else set.add(arr[i])
+    
+// }
+// console.log(set);
+
+// check sentence is pangilarm
+//The quick brown fox jumps over the lazy dog
+
+let sen="thequickbrownfoxjumpsoverthelazydog"
+let set=new Set()
+for (let i = 0; i < sen.length; i++) {
+   let ch=sen.charAt(i);
+   set.add(ch)
     
 }
 console.log(set);
+console.log(set.size);
+
+
+if (set.size===26) {
+    console.log(true);
+    
+    
+}
