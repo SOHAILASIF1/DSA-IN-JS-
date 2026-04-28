62787620dee84a678f76f33c7d59f94f6cace2e6
SOHAILASIF1 <alyana998877@gmail.com>
2026-04-28 07:13:05 +0500

    auto-save

--- linked list/index.js
+++ linked list/index.js
@@ -96,7 +96,9 @@ let tailnode = new node(4)
 insertatend(tailnode, head)
 
 //search in linked list
-let ans=searchin(head)
+let ans=searchin(head,3)
+console.log(ans);
+
 
 //delete at end
 //  head=deleteatend(head)
