e8c2ea0c594743e4c704c7b6484eb676855fab32
SOHAILASIF1 <alyana998877@gmail.com>
2026-05-10 11:21:05 +0500

    auto commit

--- stack/index.js
+++ stack/index.js
@@ -11,9 +11,21 @@ function pop(data) {
     
     
 }
+function top() {
+    return stack.top()
+    
+}
+function top() {
+    return stack[stack.length-1]
+    
+}
+function  top(params) {
+    return 
+    
+}
 
 stack.push(100)
 stack.push(200)
 
 
-console.log(stack.pop());
+console.log(stack.top());
