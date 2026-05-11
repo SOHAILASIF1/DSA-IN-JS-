0ae893680b09fd1051fee6fd4e74c43bdd84bd13
SOHAILASIF1 <alyana998877@gmail.com>
2026-05-11 22:53:58 +0500

    auto commit

--- node.js
+++ node.js
@@ -1,6 +1,15 @@
-//node js architecture
-import fs from "fs"
-fs.readFile("test.txt","utf-8",(err,data)=>{
-    console.log(data);
+// //node js architecture
+// import fs from "fs"
+// fs.readFile("test.txt","utf-8",(err,data)=>{
+//     console.log(data);
     
-})
+// })
+let arr = [1,1,2,3,3,3]
+
+let freq = new Map()
+
+for(let num of arr){
+    freq.set(num, (freq.get(num) || 0) + 1)
+}
+
+console.log(freq);
